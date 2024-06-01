7d60673cf1e50d021f5c97a893b62e51954a237f
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-06-01 18:55:00 +0530

    fixed header top and added scroll

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const HeaderComponent = () => {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div>
+    <div
+      className={`fixed top-0 w-full transition-all ${
+        scrolled ? "bg-slate-900 shadow-md" : "bg-slate-900"
+      }`}
+    >
       <h1 className="text-2xl text-center my-4">First Issue</h1>
     </div>
   );
--- src/components/repository/repository.component.tsx
+++ src/components/repository/repository.component.tsx
@@ -3,7 +3,7 @@ import RepositoryListComponent from "./repository-list.component";
 
 const RepositoryComponent = () => {
   return (
-    <div className="flex flex-wrap md:flex-nowrap mx-5">
+    <div className="flex flex-wrap md:flex-nowrap mx-5 mt-16">
       <div className="w-full md:w-3/10 bg-lightblue">H1</div>
       <div className="w-full md:w-7/10 bg-lightgreen">
         <RepositoryListComponent />
