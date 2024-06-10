42a78a7e112639e3c050b29f53884098370702ca
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-06-11 02:15:13 +0530

    added contributing file add issue template

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
+import withPWA from "next-pwa";
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const pwaConfig = {
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+};
+
+const nextConfig = withPWA(pwaConfig);
+
+export default nextConfig = {
   output: "export",
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
 };
-
-export default nextConfig;
